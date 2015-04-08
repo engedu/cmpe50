e9a581f65805ab37a8c00c2a9f5688c221983de5
chris.hou <chrishou118@gmail.com>
2015-04-07 17:25:47 -0700

    add miterm2 cheat sheet

---
+++ Midterm2CheatSheet/src/Midterm2CheatSheet.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : Midterm2CheatSheet.cpp
+// Author      : Yutao Hou
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// c-string
+	strlen ("hello, world"); // 12
+	// pointer
+	int *p1, **p2;
+	p1 = new int;
+	p2 = new int*;
+	delete p1;
+	delete [] p2;
+
+	return 0;
+}
